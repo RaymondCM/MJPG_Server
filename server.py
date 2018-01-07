5c427e86ea3b0f198c0ffb7fd556bbe7ee133218
RaymondKirk <ray.tunstill@live.co.uk>
2018-01-07 23:12:15 +0000

    Updated camera preview logic to display at 60 frames a second

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 import socket
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from SocketServer import ThreadingMixIn
+from urlparse import urlparse
 from camera import Camera
 from time import sleep
 
@@ -13,14 +14,16 @@ class CamHandler(BaseHTTPRequestHandler):
         BaseHTTPRequestHandler.__init__(self, *args)
 
     def do_GET(self):
-        if self.path.endswith('.mjpg'):
+        url = urlparse("http://{}:{}{}".format(self.client_address[0], self.client_address[1], self.path))
+        if url.path.endswith('.mjpg'):
             while True:
                 try:
                     self.send_response(200)
                     self.send_header('Content-type', 'text/jpeg')
                     self.end_headers()
                     self.wfile.write(self.capture.get_frame())
-                    sleep(0.05)
+                    if url.query.startswith("dt="):
+                        break
                 except socket.error as ex:
                     print("Pipe closed: " + ex.args[1] if len(ex.args >= 2) else "")
                     break
