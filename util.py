c2e2debe96fdd75f966c262056deacc04b8dd04f
RaymondKirk <ray.tunstill@live.co.uk>
2018-01-07 20:09:51 +0000

    Initial commit of threaded HTTP camera server

---
+++ util.py
@@ -0,0 +1,17 @@
+from sys import platform
+
+
+def is_linux():
+    return platform == "linux" or platform == "linux2"
+
+
+def is_windows():
+    return platform == "win32" or platform == "cygwin"
+
+
+def is_osx():
+    return platform == "darwin"
+
+
+def is_bsd():
+    return platform == "freebsd7" or platform == "freebsd8" or platform == "freebsdN"
